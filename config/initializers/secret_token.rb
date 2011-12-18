22bd47bee2c09a0f9cda363372d4b855ed7fbcf1
John Maxwell <jgwmaxwell@gmail.com>
2011-12-18 16:33:15 +0000

    initial rails generation only

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatter::Application.config.secret_token = '46affd1187bf26e5b03da6e0cda82ef2ec0b215c8f60f32297662a942060c7e87645f5982403d72e72b23bea8853b8498e253a5d9ffdf9120e0077ccf5bc89b8'
