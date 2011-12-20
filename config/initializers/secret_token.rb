4cd6c90055fde7faf95bece50ea5b5520192df9f
John Maxwell <jgwmaxwell@gmail.com>
2011-12-20 01:16:03 +0000

    included the new rails 3.2 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatter32::Application.config.secret_token = '9645a6577802edc10d4abd43f32b935495a324eace1f6dd2a1b3e2ec55b5b4ead8bf610d4d91bee1b8bd8dc3c548a19ae7d6b914affb8960d1b1cf4e56979768'
